49de5d163c48ac752b8773c0885ba6075b8d861d
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2022-11-30 00:56:41 -0300

    Dashbord refeito, falta liga as telas.

--- app/src/main/java/com/example/kidsoficial/DashboardActivity.java
+++ app/src/main/java/com/example/kidsoficial/DashboardActivity.java
@@ -1,13 +1,19 @@
 package com.example.kidsoficial;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DashboardActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_dashboard );
+
+        Intent in = new Intent(DashboardActivity.this, DashboardActivity.class);
+        startActivity(in);
+
     }
 
 }
